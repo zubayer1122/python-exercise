0ef59fad677f6402d623431d9d4a578517a328d5
zubayer1122 <ahmed25801415@gmail.com>
2025-09-28 10:41:46 -0700

    first commit

---
+++ class1.py
@@ -0,0 +1,32 @@
+#Python Operators:-
+#addition
+x = 10
+y = 20
+print( x + y )
+#Subtraction
+a = 12
+b = 10
+print(a-b)
+#Multiplication
+c = 4
+d = 5
+print(c*d)
+#Division
+e = 8
+f = 5
+print(e/f)
+#Modulus
+g = 55
+h = 3
+print(g%h)
+#Exponentiation
+i = 5
+j = 2
+print(i**j)
+#Floor division
+k = 4
+l = 1187
+print(k//l)
+
+
+
